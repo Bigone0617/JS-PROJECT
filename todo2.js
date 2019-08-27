ac4d73e84a736836ff772ed1393eee34dd9218e7
Bigone0617 <xodlfsha@gmail.com>
2019-08-27 23:42:07 +0900

    init

---
+++ todo2.js
@@ -0,0 +1,9 @@
+/*const toDoform = document.querySelector(".js-toDoForm"),
+      toDoinput = toDoform.querySelector("input"),
+      toDoList = document.querySelector("js-toDoList");
+
+function init(){
+    
+}
+
+init();*/
